57438fd7f85cb75c387cde813eff511b6447c670
Aleksandar Vracarevic <malimeda@live.com>
2025-04-16 00:31:48 +0200

    Update ADV to use extended API.

--- testing/integration/rpa_psa_integration/src/main.c
+++ testing/integration/rpa_psa_integration/src/main.c
@@ -55,12 +55,6 @@ ZTEST(bt_psa_integration, test_rpa_creation_and_resolution)
 ZTEST(bt_psa_integration, test_advertising_address_is_rpa) {
 	int err;
 
-	// LOG_INF("Enabling the bt stack");
-	// err = bt_enable(NULL);
-	// zassert_equal(err, 0, "Bluetooth init failed (err %d)", err);
-
-	// settings_load();
-
 	uint8_t irk[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xDE, 0xAD, 0xBE, 0xEF,
 	                 0xDE, 0xAD, 0xBE, 0xEF, 0xDE, 0xAD, 0xBE, 0xEF};
 
@@ -71,7 +65,7 @@ ZTEST(bt_psa_integration, test_advertising_address_is_rpa) {
     bt_id_reset(id, NULL, irk);
 
 	k_sleep(K_SECONDS(1));
-
+	struct bt_le_ext_adv *adv;
 	const struct bt_le_adv_param adv_params = {
 		.id = id,
 		.options = BT_LE_ADV_OPT_CONN,
@@ -79,35 +73,32 @@ ZTEST(bt_psa_integration, test_advertising_address_is_rpa) {
 		.interval_max = BT_GAP_ADV_FAST_INT_MAX_2,
 	};
 
+	err = bt_le_ext_adv_create(&adv_params, NULL, &adv);
+	zassert_equal(err, 0, "Extended advertising object creation failed: %d", err);
+
 	LOG_INF("Starting advertising...");
-	err = bt_le_adv_start(&adv_params, NULL, 0, NULL, 0);
+	const struct bt_le_ext_adv_start_param ext_adv_params = {
+		.timeout = 60,
+		.num_events = 100,
+	};
+	err = bt_le_ext_adv_start(adv, &ext_adv_params);
 	zassert_equal(err, 0, "Advertising start failed: %d", err);
 
 	k_sleep(K_SECONDS(1));
 
-	struct bt_le_oob oob;
-	err = bt_le_oob_get_local(id, &oob);
-	zassert_equal(err, 0, "Failed to get OOB info");
+	struct bt_le_ext_adv_info advertising_info;
+	err = bt_le_ext_adv_get_info(adv, &advertising_info);
+	zassert_equal(err, 0, "Failed to get advertising info: %d", err);
 
-	bt_addr_le_t addrs[CONFIG_BT_ID_MAX];
-	size_t count = CONFIG_BT_ID_MAX;
+	LOG_INF("Advertising address (from advertising info): %02X:%02X:%02X:%02X:%02X:%02X (%s)",
+	        advertising_info.addr->a.val[5], advertising_info.addr->a.val[4], advertising_info.addr->a.val[3],
+	        advertising_info.addr->a.val[2], advertising_info.addr->a.val[1], advertising_info.addr->a.val[0],
+	        advertising_info.addr->type == BT_ADDR_LE_RANDOM ? "RANDOM" : "PUBLIC");
 
-	bt_id_get(addrs, &count);
-	char addr_s[BT_ADDR_LE_STR_LEN];
-	for (size_t i = 0; i < count; i++) {
-		bt_addr_le_to_str(&addrs[i], addr_s, sizeof(addr_s));
-		LOG_INF("addr: %s", addr_s);
-	}
+	zassert_true(BT_ADDR_IS_RPA(&advertising_info.addr->a), "Advertiser address is not RPA");
 
-
-	LOG_INF("Advertising address (from OOB): %02X:%02X:%02X:%02X:%02X:%02X (%s)",
-	        oob.addr.a.val[5], oob.addr.a.val[4], oob.addr.a.val[3],
-	        oob.addr.a.val[2], oob.addr.a.val[1], oob.addr.a.val[0],
-	        oob.addr.type == BT_ADDR_LE_RANDOM ? "RANDOM" : "PUBLIC");
-
-	zassert_true(BT_ADDR_IS_RPA(&oob.addr.a), "Advertiser address is not RPA");
-
-	bt_le_adv_stop();
+	bt_le_ext_adv_stop(adv);
+	bt_le_ext_adv_delete(adv);
 	// bt_disable();
 }
 
@@ -130,7 +121,7 @@ ZTEST(bt_psa_integration, test_rpa_rotation) {
     bt_id_reset(id, NULL, irk);
 
 	k_sleep(K_SECONDS(1));
-
+	struct bt_le_ext_adv *adv;
 	const struct bt_le_adv_param adv_params = {
 		.id = id,
 		.options = BT_LE_ADV_OPT_CONN,
@@ -138,56 +129,53 @@ ZTEST(bt_psa_integration, test_rpa_rotation) {
 		.interval_max = BT_GAP_ADV_FAST_INT_MAX_2,
 	};
 
+	err = bt_le_ext_adv_create(&adv_params, NULL, &adv);
+	zassert_equal(err, 0, "Extended advertising object creation failed: %d", err);
+
 	LOG_INF("Starting advertising...");
-	err = bt_le_adv_start(&adv_params, NULL, 0, NULL, 0);
+	const struct bt_le_ext_adv_start_param ext_adv_params = {
+		.timeout = 0,
+		.num_events = 0,
+	};
+	err = bt_le_ext_adv_start(adv, &ext_adv_params);
 	zassert_equal(err, 0, "Advertising start failed: %d", err);
 
 	k_sleep(K_SECONDS(1));
 
-	struct bt_le_oob oob;
-	err = bt_le_oob_get_local(id, &oob);
-	zassert_equal(err, 0, "Failed to get OOB info");
-
-	bt_addr_le_t addrs[CONFIG_BT_ID_MAX];
-	size_t count = CONFIG_BT_ID_MAX;
-
-	bt_id_get(addrs, &count);
-	char addr_s[BT_ADDR_LE_STR_LEN];
-	for (size_t i = 0; i < count; i++) {
-		bt_addr_le_to_str(&addrs[i], addr_s, sizeof(addr_s));
-		LOG_INF("addr: %s", addr_s);
-	}
+	struct bt_le_ext_adv_info advertising_info;
+	err = bt_le_ext_adv_get_info(adv, &advertising_info);
+	zassert_equal(err, 0, "Failed to get advertising info: %d", err);
 
 
 	LOG_INF("Advertising address (from OOB): %02X:%02X:%02X:%02X:%02X:%02X (%s)",
-	        oob.addr.a.val[5], oob.addr.a.val[4], oob.addr.a.val[3],
-	        oob.addr.a.val[2], oob.addr.a.val[1], oob.addr.a.val[0],
-	        oob.addr.type == BT_ADDR_LE_RANDOM ? "RANDOM" : "PUBLIC");
+	        advertising_info.addr->a.val[5], advertising_info.addr->a.val[4], advertising_info.addr->a.val[3],
+	        advertising_info.addr->a.val[2], advertising_info.addr->a.val[1], advertising_info.addr->a.val[0],
+	        advertising_info.addr->type == BT_ADDR_LE_RANDOM ? "RANDOM" : "PUBLIC");
+	
+	bt_addr_le_t addr_before = *advertising_info.addr;
+	zassert_true(BT_ADDR_IS_RPA(&advertising_info.addr->a), "Advertiser address is not RPA");
 
-	zassert_true(BT_ADDR_IS_RPA(&oob.addr.a), "Advertiser address is not RPA");
-
-	uint16_t rpa_short_timeout = 3;
+	uint16_t rpa_short_timeout = 1;
 	err = bt_le_set_rpa_timeout(rpa_short_timeout);
 	zassert_equal(err, 0, "Failed to set RPA timeout");
-	LOG_INF("Sleeping for RPA timeout...");
-	k_sleep(K_SECONDS(rpa_short_timeout + 1));
+	LOG_INF("Sleeping for RPA timeout [%d]...", rpa_short_timeout);
+	k_sleep(K_SECONDS(rpa_short_timeout * 35));
 
-	struct bt_le_oob oob_after;
-	err = bt_le_oob_get_local(id, &oob_after);
-	zassert_equal(err, 0, "Failed to get new OOB address");
-	bt_addr_le_t addr_after = oob_after.addr;
+	err = bt_le_ext_adv_get_info(adv, &advertising_info);
+	zassert_equal(err, 0, "Failed to get advertising info: %d", err);
+	bt_addr_le_t addr_after = *advertising_info.addr;
 
 	LOG_INF("Rotated address: %02X:%02X:%02X:%02X:%02X:%02X",
 		addr_after.a.val[5], addr_after.a.val[4], addr_after.a.val[3],
 		addr_after.a.val[2], addr_after.a.val[1], addr_after.a.val[0]);
 
-	zassert_true(BT_ADDR_IS_RPA(&oob_after.addr.a), "Advertiser address is not RPA");
+	zassert_true(BT_ADDR_IS_RPA(&addr_after.a), "Advertiser address is not RPA");
 
-	zassert_true(memcmp(oob.addr.a.val, addr_after.a.val, sizeof(bt_addr_t)) != 0,
+	zassert_true(memcmp(addr_before.a.val, addr_after.a.val, sizeof(bt_addr_t)) != 0,
 	                      "RPA did not rotate");
 
-	bt_le_adv_stop();
-	// bt_disable();
+	bt_le_ext_adv_stop(adv);
+	bt_le_ext_adv_delete(adv);
 }
 
 ZTEST(bt_psa_integration, test_rpa_is_stable_before_timeout)
@@ -198,54 +186,122 @@ ZTEST(bt_psa_integration, test_rpa_is_stable_before_timeout)
 	int id = bt_id_create(NULL, irk);
 	zassert_true(id >= 0, "ID creation failed");
 
+	struct bt_le_ext_adv *adv;
 	struct bt_le_adv_param adv_params = {
 		.id = id,
 		.options = BT_LE_ADV_OPT_CONN,
 		.interval_min = BT_GAP_ADV_FAST_INT_MIN_2,
 		.interval_max = BT_GAP_ADV_FAST_INT_MAX_2,
 	};
-	err = bt_le_adv_start(&adv_params, NULL, 0, NULL, 0);
+
+	err = bt_le_ext_adv_create(&adv_params, NULL, &adv);
+	zassert_equal(err, 0, "Extended advertising object creation failed: %d", err);
+
+	LOG_INF("Starting advertising...");
+	const struct bt_le_ext_adv_start_param ext_adv_params = {
+		.timeout = 60,
+		.num_events = 100,
+	};
+	err = bt_le_ext_adv_start(adv, &ext_adv_params);
 	zassert_equal(err, 0, "Adv start failed");
 
 	// Set timeout to something long
-	// err = bt_le_set_rpa_timeout(10);
-	// zassert_equal(err, 0, "RPA timeout set failed");
-	bt_addr_le_t addrs[CONFIG_BT_ID_MAX];
-	size_t count = CONFIG_BT_ID_MAX;
+	err = bt_le_set_rpa_timeout(30 * 2);
+	zassert_equal(err, 0, "RPA timeout set failed");
 
-	bt_id_get(addrs, &count);
-	char addr_s[BT_ADDR_LE_STR_LEN];
-	for (size_t i = 0; i < count; i++) {
-		bt_addr_le_to_str(&addrs[i], addr_s, sizeof(addr_s));
-		LOG_INF("[bt_id_get]addr: %s", addr_s);
-	}
+	struct bt_le_ext_adv_info advertising_info;
+	err = bt_le_ext_adv_get_info(adv, &advertising_info);
+	zassert_equal(err, 0, "Failed to get advertising info: %d", err);
 
-	// struct bt_le_oob oob1, oob2;
-	// bt_le_oob_get_local(id, &oob1);
-	// k_sleep(K_SECONDS(1));
-	// bt_le_oob_get_local(id, &oob2);
+	LOG_INF("Advertising address (from advertising info): %02X:%02X:%02X:%02X:%02X:%02X (%s)",
+	        advertising_info.addr->a.val[5], advertising_info.addr->a.val[4], advertising_info.addr->a.val[3],
+	        advertising_info.addr->a.val[2], advertising_info.addr->a.val[1], advertising_info.addr->a.val[0],
+	        advertising_info.addr->type == BT_ADDR_LE_RANDOM ? "RANDOM" : "PUBLIC");
 
-	// Stop advertising before assertion
-	bt_le_adv_stop();
-	// zassert_true(memcmp(oob1.addr.a.val, oob2.addr.a.val, sizeof(bt_addr_t)) == 0,
-	//              "RPA changed unexpectedly");
+	bt_addr_le_t addr_before = *advertising_info.addr;
+	zassert_true(BT_ADDR_IS_RPA(&advertising_info.addr->a), "Advertiser address is not RPA");
+
+	k_sleep(K_SECONDS(5));
+
+	err = bt_le_ext_adv_get_info(adv, &advertising_info);
+	zassert_equal(err, 0, "Failed to get advertising info: %d", err);
+	bt_addr_le_t addr_after = *advertising_info.addr;
+
+	LOG_INF("Rotated address: %02X:%02X:%02X:%02X:%02X:%02X",
+		addr_after.a.val[5], addr_after.a.val[4], addr_after.a.val[3],
+		addr_after.a.val[2], addr_after.a.val[1], addr_after.a.val[0]);
+
+	zassert_true(BT_ADDR_IS_RPA(&addr_after.a), "Advertiser address is not RPA");
+	
+	zassert_true(memcmp(addr_before.a.val, addr_after.a.val, sizeof(bt_addr_t)) == 0,
+	             "RPA changed unexpectedly");
+	
+	bt_le_ext_adv_stop(adv);
+	bt_le_ext_adv_delete(adv);
 }
 
 ZTEST(bt_psa_integration, test_different_irks_yield_different_rpas)
 {
-	uint8_t irk1[16] = { [0 ... 15] = 0x11 };
-	uint8_t irk2[16] = { [0 ... 15] = 0x22 };
+    int err;
+    uint8_t irk1[16] = { [0 ... 15] = 0x11 };
+    uint8_t irk2[16] = { [0 ... 15] = 0x22 };
 
-	int id1 = bt_id_create(NULL, irk1);
-	int id2 = bt_id_create(NULL, irk2);
-	zassert_true(id1 >= 0 && id2 >= 0, "Failed to create identities");
+    int id1 = bt_id_create(NULL, irk1);
+    int id2 = bt_id_create(NULL, irk2);
+    zassert_true(id1 >= 0 && id2 >= 0, "Failed to create identities");
 
-	struct bt_le_oob oob1, oob2;
-	bt_le_oob_get_local(id1, &oob1);
-	bt_le_oob_get_local(id2, &oob2);
+    struct bt_le_ext_adv *adv1, *adv2;
+    struct bt_le_ext_adv_info adv_info1, adv_info2;
+	bt_addr_le_t addr1, addr2;
 
-	zassert_false(bt_addr_le_cmp(&oob1.addr, &oob2.addr) == 0,
-	              "RPAs are unexpectedly equal");
+    const struct bt_le_adv_param adv_params1 = {
+        .id = id1,
+        .options = BT_LE_ADV_OPT_CONN,
+        .interval_min = BT_GAP_ADV_FAST_INT_MIN_2,
+        .interval_max = BT_GAP_ADV_FAST_INT_MAX_2,
+        .peer = NULL,
+    };
+
+    const struct bt_le_adv_param adv_params2 = {
+        .id = id2,
+        .options = BT_LE_ADV_OPT_CONN,
+        .interval_min = BT_GAP_ADV_FAST_INT_MIN_2,
+        .interval_max = BT_GAP_ADV_FAST_INT_MAX_2,
+        .peer = NULL,
+    };
+
+    err = bt_le_ext_adv_create(&adv_params1, NULL, &adv1);
+    zassert_equal(err, 0, "Failed to create advertising set 1: %d", err);
+
+	err = bt_le_ext_adv_get_info(adv1, &adv_info1);
+    zassert_equal(err, 0, "Failed to get advertising info 1: %d", err);
+
+	LOG_INF("Advertising address (from advertising info): %02X:%02X:%02X:%02X:%02X:%02X (%s)",
+	        adv_info1.addr->a.val[5], adv_info1.addr->a.val[4], adv_info1.addr->a.val[3],
+	        adv_info1.addr->a.val[2], adv_info1.addr->a.val[1], adv_info1.addr->a.val[0],
+	        adv_info1.addr->type == BT_ADDR_LE_RANDOM ? "RANDOM" : "PUBLIC");
+
+	bt_addr_le_copy(&addr1, adv_info1.addr);
+
+	bt_le_ext_adv_delete(adv1);
+
+    err = bt_le_ext_adv_create(&adv_params2, NULL, &adv2);
+    zassert_equal(err, 0, "Failed to create advertising set 2: %d", err);
+
+    err = bt_le_ext_adv_get_info(adv2, &adv_info2);
+    zassert_equal(err, 0, "Failed to get advertising info 2: %d", err);
+
+	LOG_INF("Advertising address (from advertising info): %02X:%02X:%02X:%02X:%02X:%02X (%s)",
+	        adv_info2.addr->a.val[5], adv_info2.addr->a.val[4], adv_info2.addr->a.val[3],
+	        adv_info2.addr->a.val[2], adv_info2.addr->a.val[1], adv_info2.addr->a.val[0],
+	        adv_info2.addr->type == BT_ADDR_LE_RANDOM ? "RANDOM" : "PUBLIC");
+
+	bt_addr_le_copy(&addr2, adv_info2.addr);			
+
+    bt_le_ext_adv_delete(adv2);
+
+	zassert_false(bt_addr_le_eq(&addr1, &addr2),
+                  "RPAs are unexpectedly equal");
 }
 
 ZTEST(bt_psa_integration, test_null_irk_generates_random_irk)
